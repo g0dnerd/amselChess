52846912710b8bc3561c203b232371adafd821af
Paul Kukowski <kukowskipaul@gmail.com>
2023-02-27 19:37:11 +0100

    Continued work on multithreading and a move ordering function to increase efficiency.

--- minimax.py
+++ minimax.py
@@ -1,5 +1,15 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor
+from collections import defaultdict
 from amsel_engine import Engine
+
+PIECE_VALUES = {
+    'pawn': 100,
+    'knight': 320,
+    'bishop': 330,
+    'rook': 500,
+    'queen': 900,
+    'king': 20000
+}
 
 
 def order_moves(state):
@@ -8,20 +18,31 @@ def order_moves(state):
         return legal_moves
     else:
         ordered_moves = []
-        for i, move in enumerate(legal_moves):
+        # First, order by captures
+        for move in legal_moves:
             if state.is_capture(move):
-                ordered_moves.insert(i, move)
-            else:
                 ordered_moves.append(move)
+        # Then, order by piece mobility
+        piece_mobility_scores = {}
+        for move in legal_moves:
+            piece = state.board.get_piece_by_square(move[0]).type
+            if piece in piece_mobility_scores:
+                piece_mobility_scores[piece] += PIECE_VALUES[piece]
+            else:
+                piece_mobility_scores[piece] = PIECE_VALUES[piece]
+        ordered_moves += sorted(legal_moves,
+                                key=lambda move: piece_mobility_scores.get(
+                                    state.board.get_piece_by_square(move[0]).type, 0), reverse=True)
         return ordered_moves
 
 
 class Minimax:
-    MAX_DEPTH = 3
-    THREADS = 4
+    MAX_DEPTH = 5
+    THREADS = 6
 
     def __init__(self):
         self.engine = Engine()
+        self.history = defaultdict(list)
 
     def minimax(self, state, depth, alpha, beta, maximizing_player, path=None):
         if path is None:
@@ -31,44 +52,48 @@ class Minimax:
         if maximizing_player:
             best_value = float('-inf')
             best_move = None
-            with ThreadPoolExecutor(max_workers=self.THREADS) as executor:
-                futures = []
-                for move in order_moves(state):
-                    new_state = state.apply_move(move[0], move[1])
-                    new_path = path + [move]
-                    futures.append(executor.submit(self.minimax, new_state, depth - 1, alpha, beta, False, new_path))
-                for future in futures:
-                    value, _ = future.result()
-                    print('Result obtained')
-                    if value > best_value:
-                        best_value = value
-                        best_move = future.arg
-                    alpha = max(alpha, value)
-                    if alpha >= beta:
-                        print('Pruning')
-                        break
+            for move in order_moves(state):
+                print(f'Processing state {state.move_history} at depth {depth}')
+                new_state = state.apply_move(move[0], move[1])
+                new_path = path + [move]
+                value, _ = self.minimax(new_state, depth - 1, alpha, beta, False, new_path)
+                if value > best_value:
+                    best_value = value
+                    best_move = move
+                alpha = max(alpha, value)
+                if alpha >= beta:
+                    print(f'Pruning {new_path} at depth {depth} with value {value}')
+                    break
             return best_value, best_move
         else:
             best_value = float('inf')
             best_move = None
-            with ThreadPoolExecutor(max_workers=self.THREADS) as executor:
-                futures = []
-                for move in order_moves(state):
-                    new_state = state.apply_move(move[0], move[1])
-                    new_path = path + [move]
-                    futures.append(executor.submit(self.minimax, new_state, depth - 1, alpha, beta, True, new_path))
-                for future in futures:
-                    value, _ = future.result()
-                    print('Result obtained')
-                    if value < best_value:
-                        best_value = value
-                        best_move = future.arg
-                    beta = min(beta, value)
-                    if alpha >= beta:
-                        print('Pruning')
-                        break
+            for move in order_moves(state):
+                new_state = state.apply_move(move[0], move[1])
+                new_path = path + [move]
+                value, _ = self.minimax(new_state, depth - 1, alpha, beta, True, new_path)
+                if value < best_value:
+                    best_value = value
+                    best_move = move
+                beta = min(beta, value)
+                if alpha >= beta:
+                    print(f'Pruning {new_path} at depth {depth} with value {value}')
+                    break
             return best_value, best_move
 
     def find_best_move(self, state):
-        _, best_move = self.minimax(state, self.MAX_DEPTH, float('-inf'), float('inf'), True)
-        return best_move
+        with ThreadPoolExecutor(max_workers=self.THREADS) as executor:
+            results = []
+            for move in state.get_valid_moves():
+                print(f'Processing move {move}')
+                result = executor.submit(self.minimax, state.apply_move(move[0], move[1]), self.MAX_DEPTH - 1,
+                                         float('-inf'), float('inf'), False, [move])
+                results.append(result)
+            best_value = float('-inf')
+            best_move = None
+            for result in results:
+                value, move = result.result()
+                if value > best_value:
+                    best_value = value
+                    best_move = move
+            return best_move
